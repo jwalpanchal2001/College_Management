2cba6d467f50709be0e41e40e124e4c1148dafa8
jwalp <jwalp@JWAL-PANCHAL>
2025-11-16 12:31:10 +0530

    Fix CSRF trusted origins for Render

--- college_management/settings.py
+++ college_management/settings.py
@@ -26,7 +26,17 @@ SECRET_KEY = 'django-insecure-q0%0z&kt_(zx7r&#1kwdx!l=x0f%b3^x&x045&rd4%9nkb5e=7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "college-management-tv1b.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://college-management-tv1b.onrender.com",
+    "https://www.your-custom-domain.com",
+]
+
 
 
 # Application definition
